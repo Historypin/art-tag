358ab612ff94c31ab507e273ea6e7aa36682b03e
Miroslav Valus <miroslav.valus@eea.sk>
2016-03-12 19:32:07 +0100

    Changed backend framework.

---
+++ src/main/java/sk/eea/arttag/config/AppConfig.java
@@ -0,0 +1,68 @@
+package sk.eea.arttag.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "sk.eea.arttag")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/bower_components/**", "/scripts/**", "/styles/**", "/fonts/**", "/font/**")
+                .addResourceLocations("/app/bower_components/", "/app/scripts/", "/app/styles/", "/app/bower_components/bootstrap/fonts/", "/app/bower_components/font-awesome/font/", ".tmp/scripts/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/app/");
+        viewResolver.setSuffix(".html");
+        viewResolver.setViewClass(InternalResourceView.class);
+        viewResolver.setExposeContextBeansAsAttributes(true);
+        return viewResolver;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        // get active profile
+        String activeProfile = System.getProperty("spring.profiles.active");
+
+        // choose different property files for different active profile
+        Resource profileResource;
+        if ("dev".equalsIgnoreCase(activeProfile)) {
+            profileResource = new ClassPathResource("dev.properties");
+        } else if ("test".equalsIgnoreCase(activeProfile)) {
+            profileResource = new ClassPathResource("test.properties");
+        } else {
+            profileResource = new ClassPathResource("prod.properties");
+        }
+
+        Resource defaultResource = new ClassPathResource("default.properties");
+        // load the property files
+        propertySourcesPlaceholderConfigurer.setLocations(defaultResource, profileResource);
+
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
---
+++ src/main/java/sk/eea/arttag/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package sk.eea.arttag.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("USER", "ADMIN")
+                .and()
+                .withUser("user").password("user").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // TODO: add CSRF protection!!!
+                .authorizeRequests()
+                    .antMatchers("/login**", "/bower_components/**", "/scripts/**", "/styles/**", "/font/**", "/fonts/**").permitAll()
+                    .antMatchers("/**").authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/login.do")
+                    .defaultSuccessUrl("/")
+                    .failureUrl("/login?error")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login?logout")
+                    .deleteCookies("JSESSIONID")
+                    .invalidateHttpSession(true)
+                .and()
+                .sessionManagement()
+                    .invalidSessionUrl("/login?timeout")
+                    .maximumSessions(1);
+    }
+}

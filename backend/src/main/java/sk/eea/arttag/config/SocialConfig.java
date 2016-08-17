f8095536e47e8884e46bccdb906a782649a03254
mva <mva@eea.sk>
2016-08-17 15:33:46 +0200

    Fixing applicationURL bug.

--- backend/src/main/java/sk/eea/arttag/config/SocialConfig.java
+++ backend/src/main/java/sk/eea/arttag/config/SocialConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.social.UserIdSource;
 import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
@@ -14,10 +15,12 @@ import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 import org.springframework.social.connect.web.ConnectController;
+import org.springframework.social.connect.web.ProviderSignInController;
 import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.social.google.connect.GoogleConnectionFactory;
 import org.springframework.social.security.AuthenticationNameUserIdSource;
 import sk.eea.arttag.service.AccountConnectionSignUpService;
+import sk.eea.arttag.service.SpringSecuritySignInAdapter;
 
 import javax.sql.DataSource;
 
@@ -34,6 +37,7 @@ public class SocialConfig implements SocialConfigurer {
     @Autowired
     private AccountConnectionSignUpService accountConnectionSignUpService;
 
+
     @Override
     public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer, Environment environment) {
         connectionFactoryConfigurer.addConnectionFactory(new FacebookConnectionFactory(
@@ -49,10 +53,31 @@ public class SocialConfig implements SocialConfigurer {
         return new AuthenticationNameUserIdSource();
     }
 
+
+
     @Bean
     public ConnectController connectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
         ConnectController controller = new ConnectController(
                 connectionFactoryLocator, connectionRepository);
+        final String applicationURL =
+                environment.getProperty("application.hostname-prefix") +
+                        environment.getProperty("application.hostname") +
+                        environment.getProperty("application.context-path");
+        controller.setApplicationUrl(applicationURL);
+
+        return controller;
+    }
+
+    @Bean
+    public ProviderSignInController providerSignInController(
+            ConnectionFactoryLocator connectionFactoryLocator,
+            UsersConnectionRepository usersConnectionRepository,
+            SpringSecuritySignInAdapter springSecuritySignInAdapter) {
+        ProviderSignInController controller = new ProviderSignInController(
+                connectionFactoryLocator,
+                usersConnectionRepository,
+                springSecuritySignInAdapter);
+
         final String applicationURL =
                 environment.getProperty("application.hostname-prefix") +
                         environment.getProperty("application.hostname") +

7818dcb03051e5824a4c10fc440864fd2a443e01
mva <mva@eea.sk>
2016-08-17 15:02:11 +0200

    Changed Facebook ID for social login, added application URL to social login.

--- backend/src/main/java/sk/eea/arttag/config/SocialConfig.java
+++ backend/src/main/java/sk/eea/arttag/config/SocialConfig.java
@@ -1,6 +1,7 @@
 package sk.eea.arttag.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.encrypt.Encryptors;
@@ -9,8 +10,11 @@ import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurer;
 import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
+import org.springframework.social.connect.web.ConnectController;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.social.google.connect.GoogleConnectionFactory;
 import org.springframework.social.security.AuthenticationNameUserIdSource;
 import sk.eea.arttag.service.AccountConnectionSignUpService;
@@ -22,6 +26,9 @@ import javax.sql.DataSource;
 public class SocialConfig implements SocialConfigurer {
 
     @Autowired
+    private Environment environment;
+
+    @Autowired
     private DataSource dataSource;
 
     @Autowired
@@ -29,17 +36,29 @@ public class SocialConfig implements SocialConfigurer {
 
     @Override
     public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer, Environment environment) {
-        //        connectionFactoryConfigurer.addConnectionFactory(new FacebookConnectionFactory(
-        //                environment.getProperty("spring.social.facebook.appId"),
-        //                environment.getProperty("spring.social.facebook.appSecret")));
+        connectionFactoryConfigurer.addConnectionFactory(new FacebookConnectionFactory(
+                        environment.getProperty("facebook.appId"),
+                        environment.getProperty("facebook.appSecret")));
         connectionFactoryConfigurer.addConnectionFactory(new GoogleConnectionFactory(
-                environment.getProperty("spring.social.google.appId"),
-                environment.getProperty("spring.social.google.appSecret")));
+                environment.getProperty("google.appId"),
+                environment.getProperty("google.appSecret")));
     }
 
     @Override
     public UserIdSource getUserIdSource() {
         return new AuthenticationNameUserIdSource();
+    }
+
+    @Bean
+    public ConnectController connectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
+        ConnectController controller = new ConnectController(
+                connectionFactoryLocator, connectionRepository);
+        final String applicationURL =
+                environment.getProperty("application.hostname-prefix") +
+                        environment.getProperty("application.hostname") +
+                        environment.getProperty("application.context-path");
+        controller.setApplicationUrl(applicationURL);
+        return controller;
     }
 
     @Override
